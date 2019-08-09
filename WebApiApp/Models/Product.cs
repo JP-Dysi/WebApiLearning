9e65beeeee28ad1d24b9cfe6caa1c41ec1eb0105
John <jpeters@dynamicsignal.com>
2019-08-09 13:39:11 +0100

    added model validation annotations

--- WebApiApp/Models/Product.cs
+++ WebApiApp/Models/Product.cs
@@ -1,13 +1,20 @@
-﻿namespace WebApiApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiApp.Models
 {
 	public class Product
 	{
 		public int Id { get; set; }
 
+		[Required]
+		[MaxLength(20)]
 		public string Name { get; set; }
-		
-		public string Category { get; set; }
 
+		[Required]
+		public string Category { get; set; }
+		
+		[Required]
+		[Range(0.01d, 9999d)]
 		public decimal Price { get; set; }
 	}
 }
